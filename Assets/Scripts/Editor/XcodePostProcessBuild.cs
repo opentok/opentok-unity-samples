e16174b803a78e42f97d662ff5f25ddca9ba1076
Roberto <robj.perez@gmail.com>
2019-06-18 12:57:05 +0200

    Improve iOS build support. Auto-add VideoToolbox framework

---
+++ Assets/Scripts/Editor/XcodePostProcessBuild.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+
+public class XcodePostProcessBuild
+{
+    [PostProcessBuildAttribute(1)]
+     public static void OnPostProcessBuild(BuildTarget target, string path)
+     {
+         if (target == BuildTarget.iOS)
+         {
+             // Read.
+             string projectPath = PBXProject.GetPBXProjectPath(path);
+             PBXProject project = new PBXProject();
+             project.ReadFromString(File.ReadAllText(projectPath));
+             string targetName = PBXProject.GetUnityTargetName(); // note, not "project." ...
+             string targetGUID = project.TargetGuidByName(targetName);
+
+             AddFrameworks(project, targetGUID);
+
+             // Write.
+             File.WriteAllText(projectPath, project.WriteToString());
+         }
+     }
+
+     static void AddFrameworks(PBXProject project, string targetGUID)
+     {
+         project.AddFrameworkToProject(targetGUID, "VideoToolbox.framework", false);
+     }
+}
